de7920429cf4f029dc1bb6b540943057036ef36f
Tanishq48 <112100855+Tanishq48@users.noreply.github.com>
2022-09-28 18:31:58 +0530

    Update Week 4.sol

--- Week 4.sol
+++ Week 4.sol
@@ -8,21 +8,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 interface ERC20transfer {
-    function transfer(address _to, uint256 _value) external returns (bool success);//for withdrawing tokens tby those 5 accounts
+    function transfer(address _to, uint256 _value) external returns (bool success);  //for withdrawing tokens by those 5 accounts
 }
 
 contract My721Token is ERC721, Ownable {
 
     uint NFTsminted=0;
 
-    ERC20transfer public ERC20contractAddress;//address of the erc20 contract wo withdraw tokens
+    ERC20transfer public ERC20contractAddress;  //address of the erc20 contract wo withdraw tokens
 
     mapping(uint => address) public NFTowners;
     mapping(address => uint) public numberofNFT;
 
     constructor() ERC721("my721Token", "M7T") {}
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner { //for minting tokens to 5 different tokens
+    function safeMint(address to, uint256 tokenId) public onlyOwner {   //for minting tokens to 5 different tokens
         require(numberofNFT[to] < 1 ,"Only 1 NFT can be minted for 1 account");
         require(NFTsminted < 5 , "No more NFTs can be minted");
         _safeMint(to, tokenId);
@@ -32,7 +32,7 @@ contract My721Token is ERC721, Ownable {
     }
 
     function updateContractAddress(address contractAddress) public onlyOwner{ 
-        ERC20contractAddress = ERC20transfer(contractAddress); //making a variable ERC20transfer(interface used) type
+        ERC20contractAddress = ERC20transfer(contractAddress);  //making a variable ERC20transfer(interface used) type
     }
 
     function withdrawTokens() public payable {
